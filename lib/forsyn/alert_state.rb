581b63abba5c34f69fea78f7f9530a4a648a6e12
ragmaanir <ragmaanir@gmail.com>
2014-06-27 02:16:27 +0200

    Refactored AlertState

--- lib/forsyn/alert_state.rb
+++ lib/forsyn/alert_state.rb
@@ -9,19 +9,16 @@ module Forsyn
 
   public
 
-    ALARM   = AlertState.new(3)
-    WARN    = AlertState.new(2)
-    INFO    = AlertState.new(1)
-    NORMAL  = AlertState.new(0)
-    UNKNOWN = AlertState.new(-1)
+    CRITICAL = AlertState.new(3)
+    ABNORMAL = AlertState.new(2)
+    NOTABLE  = AlertState.new(1)
+    NORMAL   = AlertState.new(0)
+    UNKNOWN  = AlertState.new(-1)
 
     attr_reader :value
 
     def <=>(other)
-      case other
-        when AlertState then value <=> other.value
-        else false
-      end
+      value <=> other.value if other.is_a?(AlertState)
     end
 
   end
