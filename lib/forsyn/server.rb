f5f6da908ae97f124c70d53d428aebc8a7b8f1fb
ragmaanir <ragmaanir@gmail.com>
2015-06-28 16:13:53 +0200

    Cleaned up server class

--- lib/forsyn/server.rb
+++ lib/forsyn/server.rb
@@ -3,10 +3,12 @@ module Forsyn
 
     class Connection < EventMachine::Connection
 
-      attr_accessor :event_queue
+      def configure(event_queue:, logger: Forsyn.logger)
+        @event_queue = event_queue
+        @logger = logger
+      end
 
       def receive_data(data)
-        puts data
         send_data({status: 200}.to_json)
 
         @event_queue.push(data)
@@ -28,38 +30,59 @@ module Forsyn
       end
     end
 
-    def initialize(host: '127.0.0.1', port: 7331, ssl: nil)
+    attr_reader :host, :port, :logger
+
+    def initialize(host: '127.0.0.1', port: 7331, ssl: nil, logger: Forsyn.logger)
       @host = host
       @port = port
       #@socket = TCPSocket.new(host, port)
       #@tls_socket = OpenSSL::SSL::SSLSocket.new(@socket, ctx)
+      @logger = logger
+      @running = false
+
+      @event_backend = EventBackends::ElasticsearchBackend.new
+      @event_queue = Queue.new
+
+      @dispatcher = BufferedDispatcher.new(@event_queue) do |events|
+        @event_backend.store(events)
+      end
     end
 
     def run
+      raise "Already running" if @running
+      @running = true
+
       EventMachine.run do
 
-        event_backend = EventBackends::ElasticsearchBackend.new
-        event_queue = Queue.new
+        trap_signals
 
-        dispatcher = BufferedDispatcher.new(event_queue) do |events|
-          event_backend.store(events)
-        end
-
-        dispatcher.run
-
-        %w{SIGTERM SIGKILL INT}.each do |sig|
-          Signal.trap(sig) {
-            EventMachine.stop
-            dispatcher.stop
-            puts "STOPPED"
-          }
-        end
+        @dispatcher.run
 
         EventMachine.start_server(@host, @port, Connection) do |conn|
-          conn.event_queue = event_queue
+          conn.configure(event_queue: @event_queue)
         end
 
-        puts "STARTED"
+        logger.info "Started"
+      end
+
+      logger.info "Stopped"
+    end
+
+  private
+
+    def stop
+      if @running
+        @running = false
+        EventMachine.stop
+        @dispatcher.stop
+      end
+    end
+
+    def trap_signals
+      %w{TERM INT}.each do |sig|
+        Signal.trap(sig) {
+          stop
+        }
       end
     end
 
